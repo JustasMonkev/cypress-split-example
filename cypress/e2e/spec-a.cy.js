b461b55571e6e7ea001b41d08a5e9cd71bfaa6c6
JustasM <justas.monkev@gmail.com>
2023-03-15 20:50:39 +0200

    updates

--- cypress/e2e/spec-a.cy.js
+++ cypress/e2e/spec-a.cy.js
@@ -3,10 +3,11 @@
 /// <reference types="cypress" />
 
 let arr = [1,2,3,4,5,6,7,8,9,10];
+let str = ['A1', 'A2']
 
 for(let i = 0; i < 10; i++) {
 it('runs test A1', () => {
     cy.wait(1000);
-    console.log('test A1');
+    console.log('test A1 ' + str[i]);
 })
 }
