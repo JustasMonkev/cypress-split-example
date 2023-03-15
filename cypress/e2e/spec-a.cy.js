51b3e5e891c6cdc702b9ff28d6b8ae0dab4ab905
JustasM <justas.monkev@gmail.com>
2023-03-15 21:40:02 +0200

    update array

--- cypress/e2e/spec-a.cy.js
+++ cypress/e2e/spec-a.cy.js
@@ -3,7 +3,7 @@
 /// <reference types="cypress" />
 
 let arr = [1,2,3,4,5,6,7,8,9,10];
-let str = ['A1', 'A2']
+let str = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10']
 
 for(let i = 0; i < 10; i++) {
 it('runs test A1', () => {
