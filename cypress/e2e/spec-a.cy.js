5cfa69981d268479d1e44b0003fb9d9ecc234bef
JustasM <justas.monkev@gmail.com>
2023-03-15 20:33:08 +0200

    bs

--- cypress/e2e/spec-a.cy.js
+++ cypress/e2e/spec-a.cy.js
@@ -2,6 +2,8 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
 for(let i = 0; i < 10; i++) {
 it('runs test A1', () => {
     cy.wait(1000);
